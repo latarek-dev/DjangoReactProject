947d75cfe890fa16afc6ed447c0e960a49a7f079
Filip <flips.talarek17@gmail.com>
2023-05-16 01:17:34 +0200

    Logging and logout working

--- backend/polls/serializers.py
+++ backend/polls/serializers.py
@@ -8,8 +8,9 @@ from rest_framework.response import Response
 """
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
-from rest_framework import serializers, generics
-from django.utils import timezone
+from rest_framework import serializers, generics, status
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,6 +33,30 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, style={'placeholder': 'Username'})
+    password = serializers.CharField(max_length=128, write_only=True, style={'input_type': 'password', 'placeholder': 'Password'})
+
+    def validate(self, attrs):
+        # Take username and password from request
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            # Try to authenticate the user using Django auth framework.
+            user = authenticate(request=self.context.get('request'), username=username, password=password)
+            if not user:
+                # If we don't have a regular user, raise a ValidationError
+                msg = 'Access denied: wrong username or password.'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Both "username" and "password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+        # We have a valid user, put it in the serializer's validated_data.
+        # It will be used in the view.
+        attrs['user'] = user
+        return attrs
 
 """
 class UserSerializer(serializers.ModelSerializer):
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,12 +1,51 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import permissions
+from rest_framework import views
+from rest_framework.response import Response
+from . import serializers
+from django.contrib.auth import login, logout
+from rest_framework import status
+from rest_framework.views import APIView
 
 
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = []
+    serializer_class = serializers.UserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class UserLoginView(APIView):
+    # This view should be accessible also for unauthenticated users.
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = serializers.LoginSerializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+
+        user_data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'password': user.password,
+            'created_at': str(user.date_joined)
+        }
+
+        return Response(user_data, status=status.HTTP_202_ACCEPTED)
+        
+
+class LogoutView(views.APIView):
+
+    def post(self, request, format=None):
+        logout(request)
+        return Response({'success': 'Logged out successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class UsersList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
 
 """from django.shortcuts import render
 from rest_framework import generics, status
